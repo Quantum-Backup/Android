3680d1f5ee3b94645115a73e68d533b44870f63a
Zero <hello@enumerate.dev>
2023-12-03 19:56:15 -0600

    Make MainActivity do some stuff and add a StorageOverview activity

--- app/src/main/java/dev/enumerate/quantum/MainActivity.kt
+++ app/src/main/java/dev/enumerate/quantum/MainActivity.kt
@@ -1,28 +1,38 @@
 package dev.enumerate.quantum
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import dev.enumerate.quantum.ui.theme.QuantumTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             QuantumTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.surface
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +40,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuantumTheme {
-        Greeting("Android")
+fun Greeting() {
+    val context = LocalContext.current
+    Column(
+        verticalArrangement = Arrangement.spacedBy(
+            space = 48.dp,
+            alignment = Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Welcome to better, more free, storage.",
+            style = MaterialTheme.typography.displayMedium,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.fillMaxWidth(0.9f)
+        )
+        Button(onClick = {
+            context.startActivity(Intent(context, StorageOverview::class.java))
+        }) {
+            Text(text = "Start", style = MaterialTheme.typography.bodyLarge)
+        }
     }
 }
+
+//@Preview(
+//    showBackground = true
+//)
+//@Composable
+//fun GreetingPreview() {
+//    Greeting()
+//}
